ec8cef7afacde24fbb0fc3c6e2e41658dc8d7e4d
An Minh <trinhminhan1996@gmail.com>
2018-12-08 17:48:31 +0700

    add entity dangKyHoc

---
+++ src/app/_models/trang-thai-dang-ky.ts
@@ -0,0 +1,4 @@
+export class TrangThaiDangKy {
+    IDKhoaHoc: number;
+    TenKhoaHoc: string;
+}
--- src/app/quan-ly-dang-ky-hoc/danh-sach-dang-ky-hoc/danh-sach-dang-ky-hoc.component.ts
+++ src/app/quan-ly-dang-ky-hoc/danh-sach-dang-ky-hoc/danh-sach-dang-ky-hoc.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DangKyHoc} from '../../_models/dang-ky-hoc';
+import {KhoaHocHienThi} from '../../_models/khoa-hoc-hien-thi';
+import {TrangThaiDangKy} from '../../_models/trang-thai-dang-ky';
 
 @Component({
-  selector: 'app-danh-sach-dang-ky-hoc',
-  templateUrl: './danh-sach-dang-ky-hoc.component.html',
-  styleUrls: ['./danh-sach-dang-ky-hoc.component.scss']
+    selector: 'app-danh-sach-dang-ky-hoc',
+    templateUrl: './danh-sach-dang-ky-hoc.component.html',
+    styleUrls: ['./danh-sach-dang-ky-hoc.component.scss']
 })
 export class DanhSachDangKyHocComponent implements OnInit {
+    keHoachs: DangKyHoc[] = [];
+    tinhTrangs: TrangThaiDangKy[] = [];
+    khoaHocs: KhoaHocHienThi[] = [];
 
-  constructor() { }
+    total: number;
+    page: number;
+    size: number;
+    totalPage: number;
+    idTrangThai: number;
 
-  ngOnInit() {
-  }
+    searchFilter = {
+        tenKeHoach: '',
+        loaiKeHoach: '',
+        donVi: '',
+        nam: '',
+        namHoc: ''
+    };
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    getKeHoachs(): void {
+
+    }
+
+    setItemPerPage(itemPerPage: number) {
+        this.size = itemPerPage;
+        this.page = 0;
+        this.getKeHoachs();
+    }
+
+    arrayOne(): any[] {
+        return Array(this.totalPage);
+    }
+
+    changePage(page: number) {
+        this.page = page - 1;
+        this.getKeHoachs();
+    }
+
+    previousPage() {
+        this.page--;
+        this.getKeHoachs();
+    }
+
+    nextPage() {
+        this.page++;
+        this.getKeHoachs();
+    }
+
 
 }
