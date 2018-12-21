ee744998e20eb4a0ebe2664492852404e1a27637
at-thaile <thai.le@asiantech.vn>
2018-12-21 20:31:53 +0700

    them-hoc-vien-vao-kh

--- src/app/quan-ly-hoc-vien/them-hoc-vien/them-hoc-vien.component.ts
+++ src/app/quan-ly-hoc-vien/them-hoc-vien/them-hoc-vien.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { KhoaHoc } from 'app/_models/khoahoc.model';
+import { KhoahocService } from 'app/_services/khoahoc.service';
+import { QuanlihocService } from 'app/_services/quanlihoc.service';
+import { SharingService } from 'app/_services/sharing.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-them-hoc-vien',
@@ -7,9 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemHocVienComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  PageSize: 50;
+  formAddHocVien: FormGroup;
+  listKhoaHoc: KhoaHoc[] = [];
+
+  constructor(
+    private khoahocService: KhoahocService,
+    private quanlihocService: QuanlihocService,
+    private sharingService: SharingService,
+    private router: Router,
+    private fb: FormBuilder
+  ) { 
+    this.getListKhoaHoc();
   }
 
+  ngOnInit() {
+    this.formAddHocVien = this.fb.group({
+      Ho: ['', Validators.required],
+      Ten: ['', Validators.required],
+      Email: ['', Validators.required],
+      SoDienThoai: ['',Validators.required],
+      DaNopTien: [false, Validators.required],
+      SoTietDaHoc: ['0'],
+      IdKhoaHoc: [Validators.required]
+
+    })
+  }
+
+  onSubmit() {
+    this.quanlihocService.add(this.formAddHocVien.value).subscribe(
+      result => {
+        if(result['IsSuccess'] === true) {
+          this.sharingService.notifInfo("Thêm học viên vào khoá học thành công");
+          this.router.navigate(['/khoa-hoc']);
+        } else {
+          this.sharingService.notifError('Thêm học viên vào khoá học thất bại');
+        }
+      }, error => {
+        this.sharingService.notifError(error);
+      }
+    )
+  }
+
+  getListKhoaHoc() {
+    this.khoahocService.getListKhoaHoc(null).subscribe(
+      result => {
+        this.listKhoaHoc = result['Data']['CourseList'];
+      },
+      error => {
+        alert(error);
+      }
+    )
+  }
 }
